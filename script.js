560fd34a2fc2d9662161be280df96f18dba33a78
Kamila <kamilanawrat@gmail.com>
2023-09-30 00:30:34 +0200

    intersection observer

--- script.js
+++ script.js
@@ -49,3 +49,28 @@ const handleScrollSpy = () => {
 
 
 window.addEventListener('scroll', handleScrollSpy)
+
+
+const sections = document.querySelectorAll('.section');
+const menuItems = document.querySelectorAll('a');
+ 
+const options = {
+    threshold: [0.5, 0.9]
+};
+ 
+const handleScrollspy = entries => {
+    entries.forEach(entry => {
+        const activeNav = document.querySelector(`a[href='#${entry.target.id}']`);
+ 
+        if(entry.isIntersecting) {
+            menuItems.forEach(item => item.classList.remove('active'));
+            activeNav.classList.add('active');
+        };
+    });
+};
+ 
+const observer = new IntersectionObserver(handleScrollspy, options);
+ 
+sections.forEach(section => {
+    observer.observe(section);
+});
